e7dee0cfefb5fe4ba3106e672859082a038e3d63
christian-ramsland <56979108+christian-ramsland@users.noreply.github.com>
2020-04-24 00:58:00 -0400

    update3
    re-did it for equal variance and started with plots, will take some re-familiarizing myself w/ ggplot to get what we want out of it.

--- ST502 Project 3.R
+++ ST502 Project 3.R
@@ -38,10 +38,11 @@ var.test(x=USmpg,y=JapanMPG,alternative="two.sided")
 
 
 
-# Part 2 Simulation Study
+
+# Part 2 Simulation Study - equivalence 
 
 
-storeresults <- matrix(nrow = 135, ncol = 6)
+storeresults_uneq <- matrix(nrow = 135, ncol = 6)
 N = 100
 sigmasq1 <- c(1,3,9)
 sigmasq2 <- 1
@@ -50,27 +51,66 @@ sampleobs2 <- c(10,25,60)
 mew <- 0
 mewtwo <- c(-5,-1,0,1,5)
 
-count <- 0
+row <- 0
 for(i in 1:length(mewtwo)){ #mewtwo
   for(j in 1:length(sigmasq1)){
     for(k in 1:length(sampleobs1)){ #n1
       for(l in 1:length(sampleobs2)){ #n2
-        pass = 0 # counter for numbers of times it rejects H0
+        reject = 0 # counter for numbers of times it rejects H0
         for(m in 1:N){
           ynull = rnorm(n = sampleobs1[k], mean = mew, sd=sqrt(sigmasq1[j]))
           yalt = rnorm(n = sampleobs2[l], mean = mewtwo[i])
           test = t.test(ynull,yalt, var.equal = FALSE)
-          pass = ifelse((test$p.value < 0.05), pass + 1, pass)
+          reject = ifelse((test$p.value < 0.05), reject + 1, reject)
         }
-      count = count + 1
-      storeresults[count,1] = count
-      storeresults[count,2] = mewtwo[i]
-      storeresults[count,3] = sampleobs1[k]
-      storeresults[count,4] = sampleobs2[l]
-      storeresults[count,5] = sigmasq1[j]
-      storeresults[count,6] = pass
+      row = row + 1
+      # filling up the storage matrix with values needed to create plots
+      storeresults_uneq[row,1] = mewtwo[i]
+      storeresults_uneq[row,2] = sampleobs1[k]
+      storeresults_uneq[row,3] = sampleobs2[l]
+      storeresults_uneq[row,4] = sigmasq1[j]
+      storeresults_uneq[row,5] = reject
+      storeresults_uneq[row,6] = 0
       }
     }
   }
 }
+
+storeresults_eq <- matrix(nrow = 135, ncol = 6)
+row <- 0
+for(i in 1:length(mewtwo)){ #mewtwo
+  for(j in 1:length(sigmasq1)){
+    for(k in 1:length(sampleobs1)){ #n1
+      for(l in 1:length(sampleobs2)){ #n2
+        reject = 0 # counter for numbers of times it rejects H0
+        for(m in 1:N){
+          ynull = rnorm(n = sampleobs1[k], mean = mew, sd=sqrt(sigmasq1[j]))
+          yalt = rnorm(n = sampleobs2[l], mean = mewtwo[i])
+          test = t.test(ynull,yalt, var.equal = TRUE)
+          reject = ifelse((test$p.value < 0.05), reject + 1, reject)
+        }
+        row = row + 1
+        # filling up the storage matrix with values needed to create plots
+        storeresults_eq[row,1] = mewtwo[i]
+        storeresults_eq[row,2] = sampleobs1[k]
+        storeresults_eq[row,3] = sampleobs2[l]
+        storeresults_eq[row,4] = sigmasq1[j]
+        storeresults_eq[row,5] = reject
+        storeresults_eq[row,6] = 1
+      }
+    }
+  }
+}
+
+# plot construction
+
+library(ggplot2)
+
+storeresults <- as.data.frame(storeresults_uneq)
+colnames(storeresults) <- c("mu2","n1","n2","sigmasq_1","reject","eq")
+storeresults$sigmasq_1 <- as.factor(storeresults$sigmasq_1)
+
+ggplot(data = storeresults, aes(x = mu2, y = reject, group=sigmasq1)) +
+  geom_line(aes(linetype=sigmasq_1))
+
 
